f9fc4765c2a1a9969a00777832e55157f660815b
Tiat Han <ooi.tiathan@gmail.com>
2021-03-31 18:36:38 +0800

    Add firebase component

---
+++ src/components/Firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID
+})
+
+export default firebase
