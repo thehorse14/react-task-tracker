bce3dd9977010833cabcf37316fcc57ec7bdff9e
Tiat Han <ooi.tiathan@gmail.com>
2021-04-01 12:05:46 +0800

    Drop fetch from mock server and use firebase

--- src/App.js
+++ src/App.js
@@ -9,81 +9,72 @@ import SignOut from './components/SignOut'
 import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 import { useAuthState } from 'react-firebase-hooks/auth'
-import { useCollectionData  } from 'react-firebase-hooks/firestore'
 
 const auth = Firebase.auth()
 const firestore = Firebase.firestore()
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
+  const [user, setUser] = useAuthState(auth)
   const [tasks, setTasks] = useState([])
 
   useEffect(() => {
     const getTasks = async () => {
-      const tasksFromServer = await fetchTasks()
+      const tasksFromServer = user ? await fetchTasks(user) : [];
       setTasks(tasksFromServer)
     }
 
     getTasks()
-  }, [])
+  }, [user])
 
-  const baseURL = "http://localhost:5000"
+  const fetchTasks = async (user) => {
+    let snapshotArray = [];
+    const tasksRef = firestore.collection('tasks')
+    const snapshot = await tasksRef.where('userId', '==', user.uid).get()
 
-  const fetchTasks = async () => {
-    const res = await fetch(`${baseURL}/tasks`)
-    const data = await res.json()
+    if(!snapshot.empty) {
+      snapshot.forEach(doc => {
+        snapshotArray.push({...doc.data(), id: doc.id})
+      })
+    }
 
-    return data
+    return snapshotArray
   }
 
   const fetchTask = async (id) => {
-    const res = await fetch(`${baseURL}/tasks/${id}`)
-    const data = await res.json()
+    const tasksRef = firestore.collection('tasks')
+    const doc = await tasksRef.doc(id).get()
+    const data = doc.exists ? doc.data() : null
 
     return data
   }
 
   const addTask = async (task) => {
-    const res = await fetch(`${baseURL}/tasks`, {
-      method: "POST",
-      headers: {
-       'Content-type': 'application/json'
-      },
-      body: JSON.stringify(task)
-    })
+    task.userId = user.uid;
+    const data = await firestore.collection('tasks').add(task)
 
-    const data = await res.json()
-
-    setTasks([...tasks, data])
+    const addedTask = {...task, id: data.id}
+    setTasks([...tasks, addedTask])
   }
 
   const deleteTask = async (id) => {
-    await fetch(`${baseURL}/tasks/${id}`, {
-      method: 'DELETE'
-    })
+    await firestore.collection('tasks').doc(id).delete();
+
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
   const toggleReminder = async (id) => {
     const taskToToggle = await fetchTask(id)
-    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder}
-
-    const res = await fetch(`${baseURL}/tasks/${id}`, {
-      method: "PUT",
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(updatedTask)
+    const updatedReminder = !taskToToggle.reminder
+    const update = await firestore.collection('tasks').doc(id).update({
+        reminder: updatedReminder
     })
 
-    const data = await res.json()
-
+    
     setTasks(tasks.map(
-      (task) =>  task.id === id ? {...task, reminder: data.reminder} : task)
+      (task) =>  task.id === id ? {...task, reminder: updatedReminder} : task)
     )
   }
-
-  const [user] = useAuthState(auth)
 
   return (
     <Router>
@@ -96,7 +87,8 @@ const App = () => {
         <>
           <Header onAddTask={() => setShowAddTask(!showAddTask)} showAddTask={showAddTask}/>
 
-          <Route path='/' exact render={(props) => (          <>
+          <Route path='/' exact render={(props) => (          
+            <>
               {showAddTask && <AddTask onAdd={addTask}/> }
               {tasks.length > 0 ? 
                 <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> :
@@ -108,12 +100,11 @@ const App = () => {
           <Footer />
         </>
        : 
-       <>
+        <>
          <h2>Welcome to Task Tracker</h2>
          <SignIn auth={auth}/>
-       </>
-       
-       }
+        </>
+      }
       </div>
     </Router>
   );
