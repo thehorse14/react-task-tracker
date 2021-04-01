e57d754b14431dd1f020fb51d37732e6a90a2a30
Tiat Han <ooi.tiathan@gmail.com>
2021-04-01 18:58:15 +0800

    Add css for about div

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const About = () => {
     return (
-        <div>
+        <div className='about'>
             <h4>Version 1.0.0</h4>            
             <Link to="/">Go Back</Link>
         </div>
