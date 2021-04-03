02083381a46597ffbf57f5b04dc4a79c859f0aa5
Tiat Han <ooi.tiathan@gmail.com>
2021-04-04 00:25:26 +0800

    Add div to fix node insert issue after translate

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ const App = () => {
               {showAddTask && <AddTask onAdd={addTask}/> }
               {tasks.length > 0 ? 
                 <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> :
-                'No Tasks To Show'
+                <div>{'No Tasks To Show'}</div>
               }
             </>
           )} />
